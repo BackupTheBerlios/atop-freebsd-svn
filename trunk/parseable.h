c82be0bdd224418d7bdcaf0302a2d7bb0671d61f
samm <samm@262a24b7-556c-4f0b-93e0-184e8e9adcc9>
2012-12-11 09:58:30 +0000

    removing sources, migrating repository to github

--- trunk/parseable.h
+++
@@ -1,4 +0,0 @@
-int 	parsedef(char *);
-char	parseout(time_t, int, struct sstat *, struct tstat *, struct tstat **,
-                 int, int, int, int, int, int, int, int,
-	         int, unsigned int, char);
